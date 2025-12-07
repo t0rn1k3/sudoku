0e5a1e0504e5299dd076d4368a300f4a668e87e8
t0rn1k3 <tornikeburjanadze2@gmail.com>
2025-12-07 19:04:35 +0400

    create dataset for each row and column

--- scripts/scripts.ts
+++ scripts/scripts.ts
@@ -4,6 +4,12 @@ for (let row = 0; row < 9; row++) {
   for (let col = 0; col < 9; col++) {
     const cell = document.createElement("input");
     cell.classList.add("cell");
+    cell.dataset.row = String(row);
+    cell.dataset.col = String(col);
+    cell.dataset.blok = String(Math.floor(row / 3) * 3 + Math.floor(col / 3));
+    cell.type = "text";
+    cell.maxLength = 1;
+
     gameCont.appendChild(cell);
   }
 }
