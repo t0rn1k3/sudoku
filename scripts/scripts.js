88729a7fc85622e2837acd25053775087e0cbed4
t0rn1k3 <tornikeburjanadze2@gmail.com>
2025-12-11 10:07:16 +0400

    redner puzzle into cells, avoid error :   Type 'NodeListOf<HTMLInputElement>' is not an array type or a string type by puting roeCells in arra.from method

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,27 @@
 var gameCont = document.querySelector(".game-container");
 var checkButton = document.querySelector(".check-button");
+var solutionEasy = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+];
+var puzzleEasy = [
+    [5, 0, 0, 6, 0, 8, 0, 1, 0],
+    [6, 0, 2, 0, 9, 0, 3, 0, 8],
+    [0, 9, 0, 3, 0, 2, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 2, 0],
+    [0, 2, 6, 0, 5, 0, 7, 0, 1],
+    [7, 1, 0, 0, 0, 4, 0, 0, 6],
+    [0, 0, 0, 5, 0, 7, 0, 8, 4],
+    [0, 8, 0, 4, 0, 9, 6, 0, 5],
+    [3, 0, 5, 0, 8, 0, 1, 7, 0],
+];
 for (var row = 0; row < 9; row++) {
     for (var col = 0; col < 9; col++) {
         var cell = document.createElement("input");
@@ -9,11 +31,38 @@ for (var row = 0; row < 9; row++) {
         cell.dataset.blok = String(Math.floor(row / 3) * 3 + Math.floor(col / 3));
         cell.type = "number";
         cell.maxLength = 1;
+        var cellValue = puzzleEasy[row][col];
+        if (cellValue !== 0) {
+            cell.value = String(cellValue);
+            cell.disabled = true;
+            cell.classList.add("fixed");
+        }
         gameCont.appendChild(cell);
     }
 }
 var validateRow = function (rowIndex) {
     var rowCells = document.querySelectorAll(".cell[data-row=\"".concat(rowIndex, "\"]"));
-    console.log(rowCells);
+    var seen = new Set();
+    for (var _i = 0, _a = Array.from(rowCells); _i < _a.length; _i++) {
+        var cell = _a[_i];
+        var value = Number(cell.value);
+        if (!value)
+            continue;
+        if (value < 1 || value > 9) {
+            return false;
+        }
+        if (seen.has(value)) {
+            return false;
+        }
+        seen.add(value);
+    }
+    return true;
 };
-validateRow(0);
+var valudateAllRows = function () {
+    for (var r = 0; r < 9; r++) {
+        if (!validateRow(r)) {
+            return false;
+        }
+    }
+    return true;
+};
--- scripts/scripts.ts
+++ scripts/scripts.ts
@@ -3,6 +3,30 @@ const checkButton = document.querySelector(
   ".check-button"
 ) as HTMLButtonElement;
 
+const solutionEasy: number[][] = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
+];
+
+const puzzleEasy: number[][] = [
+  [5, 0, 0, 6, 0, 8, 0, 1, 0],
+  [6, 0, 2, 0, 9, 0, 3, 0, 8],
+  [0, 9, 0, 3, 0, 2, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 2, 0],
+  [0, 2, 6, 0, 5, 0, 7, 0, 1],
+  [7, 1, 0, 0, 0, 4, 0, 0, 6],
+  [0, 0, 0, 5, 0, 7, 0, 8, 4],
+  [0, 8, 0, 4, 0, 9, 6, 0, 5],
+  [3, 0, 5, 0, 8, 0, 1, 7, 0],
+];
+
 for (let row = 0; row < 9; row++) {
   for (let col = 0; col < 9; col++) {
     const cell = document.createElement("input");
@@ -12,6 +36,14 @@ for (let row = 0; row < 9; row++) {
     cell.dataset.blok = String(Math.floor(row / 3) * 3 + Math.floor(col / 3));
     cell.type = "number";
     cell.maxLength = 1;
+
+    const cellValue = puzzleEasy[row]![col];
+
+    if (cellValue !== 0) {
+      cell.value = String(cellValue);
+      cell.disabled = true;
+      cell.classList.add("fixed");
+    }
 
     gameCont.appendChild(cell);
   }
@@ -23,7 +55,7 @@ const validateRow = function (rowIndex: number) {
   );
   const seen = new Set<number>();
 
-  for (let cell of rowCells) {
+  for (let cell of Array.from(rowCells)) {
     const value = Number(cell.value);
 
     if (!value) continue;
@@ -48,27 +80,3 @@ const valudateAllRows = function (): Boolean {
   }
   return true;
 };
-
-const solutionEasy: number[][] = [
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9],
-];
-
-const puzzleEasy: number[][] = [
-  [5, 3, 0, 6, 0, 8, 0, 1, 0],
-  [6, 0, 2, 0, 9, 0, 3, 0, 8],
-  [0, 9, 8, 3, 0, 2, 5, 6, 0],
-  [8, 0, 9, 7, 6, 0, 0, 2, 0],
-  [0, 2, 6, 0, 5, 0, 7, 9, 1],
-  [7, 1, 0, 9, 0, 4, 8, 0, 6],
-  [9, 6, 0, 5, 3, 7, 0, 8, 4],
-  [0, 8, 7, 4, 0, 9, 6, 0, 5],
-  [3, 0, 5, 0, 8, 0, 1, 7, 0],
-];
